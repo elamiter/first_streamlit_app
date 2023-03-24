563dd0788b2f0b534756562a7f94f139746dd520
Arjang Farashzadeh <elamiter@hotmail.com>
2023-03-24 01:47:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,7 @@ streamlit.header('The Fruit list contains: ')
 streamlit.dataframe(my_data_rows)
 
 fruits_selected1 = streamlit.multiselect("Would you like to add friuts?", list(my_fruit_list.index))
+streamlit.text('Thanks for adding: '+ fruits_selected1)
 fruits_to_show = my_fruit_list.loc[fruits_selected1]
 streamlit.dataframe(fruits_to_show)
 
