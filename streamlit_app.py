154c6880876de4060f30f93625efd89d7745a3cd
Arjang Farashzadeh <elamiter@hotmail.com>
2023-03-24 13:51:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,11 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
+def get_fruityvice_data(x):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+x)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
@@ -29,9 +34,7 @@ try:
   if not fruit_choice:
        streamlit.error('Please select a fruit to get information!')
   else:
-       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-       fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-       streamlit.dataframe(fruityvice_normalized)
+       streamlit.dataframe(get_fruityvice_data(fruit_choice))
 except URLError as e:
     streamlit.error()
 
