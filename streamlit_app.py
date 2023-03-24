d789fec91157e8377b74286f27f76e533d4891de
Arjang Farashzadeh <elamiter@hotmail.com>
2023-03-24 13:16:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,7 @@ streamlit.header('The Fruit list contains: ')
 streamlit.dataframe(my_data_rows)
 
 add_my_fruits = streamlit.multiselect("Would you like to add friuts?", list(my_fruit_list.index))
-streamlit.write('Thanks for adding: ', str(add_my_fruits.values()))
+my_string = ', '.join(map(str,add_my_fruits))
+streamlit.write('Thanks for adding: ', my_string)
 
 my_cur.execute("insert into fruit_load_list values('from streamlit')")
